e7ca00d441e691e5f8a365c924fc2c9450db69ae
aedans <aedan.smith@outlook.com>
2025-06-24 13:05:48 -0700

    Some updates

--- nix/shared.nix
+++ nix/shared.nix
@@ -179,6 +179,8 @@
     }
   ];
 
+  zramSwap.enable = true;
+
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
