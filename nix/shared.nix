bb62eb083c895de07a7aa58ab77285f5cc2363e9
aedans <aedan.smith@outlook.com>
2025-08-05 20:09:52 -0700

    add networkmanager

--- nix/shared.nix
+++ nix/shared.nix
@@ -15,6 +15,8 @@
 
   programs.fish.enable = true;
 
+  networking.networkmanager.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
