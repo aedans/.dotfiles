6c5ae5827da14472a17864d73bfc2af3dfaf856e
aedans <aedan.smith@outlook.com>
2025-08-30 14:38:51 -0700

    Fixes

--- config/framework.nix
+++ config/framework.nix
@@ -4,5 +4,8 @@
     /home/hans/.dotfiles/nix/boot/systemd.nix
   ];
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [ "kvm.enable_virt_at_load=0" "amdgpu.sg_display=0" "amdgpu.dcdebugmask=0x410" ];
+
   networking.hostName = "framework";
 }
