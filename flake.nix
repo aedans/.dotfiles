643c467160d764f8f67c34abe09dacd379a724a7
aedans <aedan.smith@outlook.com>
2025-08-30 15:58:34 -0700

    Revert "Setup winapps"
    This reverts commit 617fbaadb05935fdbe51a3688cb0c053d70ceab3.

--- flake.nix
+++ flake.nix
@@ -7,63 +7,24 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    winapps = {
-      url = "github:winapps-org/winapps";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { 
       nixpkgs,
       home-manager,
-      winapps,
       ... 
   }@inputs: {
-    nixosConfigurations.orange = nixpkgs.lib.nixosSystem rec {
+    nixosConfigurations.orange = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-
-      specialArgs = {
-        inherit inputs system;
-      };
-
       modules = [
         ./config/orange.nix
-        (
-          {
-            pkgs,
-            system ? pkgs.system,
-            ...
-          }:
-          {
-            environment.systemPackages = [
-              winapps.packages."${system}".winapps
-            ];
-          }
-        )
       ];
     };
 
-    nixosConfigurations.framework = nixpkgs.lib.nixosSystem rec {
+    nixosConfigurations.framework = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-
-      specialArgs = {
-        inherit inputs system;
-      };
-
       modules = [
         ./config/framework.nix
-        (
-          {
-            pkgs,
-            system ? pkgs.system,
-            ...
-          }:
-          {
-            environment.systemPackages = [
-              winapps.packages."${system}".winapps
-            ];
-          }
-        )
       ];
     };
 
--- nix/shared.nix
+++ nix/shared.nix
@@ -21,7 +21,7 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  virtualisation.docker.enable = true;
+  # virtualisation.docker.enable = true;
 
   # virtualisation.vmware.host.enable = true;
 
